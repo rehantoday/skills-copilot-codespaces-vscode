aaba161fe3252b6b1d60977eb69d70b72b2546b2
rehantoday <57847874+rehantoday@users.noreply.github.com>
2023-08-11 20:49:56 +0000

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,30 +1,23 @@
 // Create web server
+
 var express = require('express');
-var app = express();
-var fs = require('fs');
-var bodyParser = require('body-parser');
-var multer = require('multer');
-var upload = multer();
+var router = express.Router();
+var commentController = require('../controllers/commentController');
+var commentValidator = require('../validators/commentValidator');
 
-// Set view engine
-app.set('view engine', 'pug');
-app.set('views', './views');
+// GET request to get all comments
+router.get('/', commentController.getComments);
 
-// Set folder public
-app.use(express.static('public'));
+// GET request for one comment
+router.get('/:id', commentController.getComment);
 
-// Use body-parser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+// POST request to create a comment
+router.post('/', commentValidator.validate('createComment'), commentController.createComment);
 
-// Use multer
-app.use(upload.array());
+// PUT request to update a comment
+router.put('/:id', commentValidator.validate('updateComment'), commentController.updateComment);
 
-// Create router
-var commentRouter = require('./router/comment.router');
-app.use('/comment', commentRouter);
+// DELETE request to delete a comment
+router.delete('/:id', commentController.deleteComment);
 
-// Create server
-app.listen(3000, function() {
-    console.log('Server is running on port 3000');
-});
+module.exports = router;
